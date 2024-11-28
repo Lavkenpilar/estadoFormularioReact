54ed198b47327a9b9a8f21342670d87111dd2598
Lavken <lavkenpilar@gmail.com>
2024-11-28 17:18:08 -0300

    import-estilos-bootstrap

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Formulario = () => {
+  return (
+    
+   <form>
+    <input name = "Nombre"/>
+    <button type='submit'>Enviar</button>
+    </form>
+   
+  )
+}
+
+export default Formulario
