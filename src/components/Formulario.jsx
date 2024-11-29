969ca7b0bb61064f6e4ca94897f736ebaab93b1c
Lavken <lavkenpilar@gmail.com>
2024-11-29 00:32:08 -0300

    create-setNombre

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -2,9 +2,14 @@ import { useState } from 'react'
 
 const Formulario = () => {
     const [nombre, setNombre] = useState ("")
+
+    const validarInput = () => {
+        alert ('evento form')
+    }
+
   return (
     
-   <form>
+   <form onSubmit={validarInput}>
     <h3>{nombre}</h3>
     <div className='form-group'>
     <input className='form-control' name = "Nombre" onChange={(e) => setNombre(e.target.value)}/>
